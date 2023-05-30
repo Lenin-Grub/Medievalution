04b1bea97ca90fcc28f675a6ba2e3451e1ad0ce7
Lening-Grib <dessotterro@mail.ru>
2023-05-30 18:31:19 +0300

    siwtch off console on release virsion

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,6 +1,11 @@
 ﻿#include "stdafx.h"
 #include "Game/Game.h"
 
+//отключает консоль на релизной версии
+#ifdef NDEBUG
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#endif
+
 int main()
 {
 	Game game;
