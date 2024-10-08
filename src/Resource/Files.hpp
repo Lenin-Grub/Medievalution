0b0dc4c6f4a65d84a8d6c1b0723beeb52bc43dde
Lenin-Grub <dessotterro@mail.ru>
2024-10-09 02:20:11 +0300

    Remove unused files

--- src/Resource/Files.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../stdafx.h"
-#include "../Common/Common.h"
-
-struct FileUtils
-{
-    static std::string getPathToFile(const std::string& filename, const std::string& filetype);
-};
