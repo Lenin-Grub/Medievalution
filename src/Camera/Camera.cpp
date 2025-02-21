6dbe3fa7b4e2af9f58421f5c05598fce80c62b43
Мурашов Георгий <murashov.george@csoft.ru>
2025-02-21 11:22:58 +0300

    Added interpolation when camera moves.

--- src/Camera/Camera.cpp
+++ src/Camera/Camera.cpp
@@ -25,14 +25,16 @@ void Camera::update(const float& dtime)
 
 void Camera::move(const float& dtime)
 {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) { common::view.move(-WindowSettings::getInstance().camera_speed * dtime, 0); }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) { common::view.move(WindowSettings::getInstance().camera_speed * dtime, 0);  }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) { common::view.move(0, -WindowSettings::getInstance().camera_speed * dtime); }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) { common::view.move(0, WindowSettings::getInstance().camera_speed * dtime);  }
+    float zoom_factor = common::view.getSize().x / WindowSettings::getInstance().resolution.width;
+    float adjusted_speed = WindowSettings::getInstance().camera_speed * zoom_factor;
 
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {common::view.move(-adjusted_speed * dtime, 0);}
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {common::view.move(adjusted_speed * dtime, 0);}
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {common::view.move(0, -adjusted_speed * dtime);}
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {common::view.move(0, adjusted_speed * dtime);}
 }
 
-void Camera::zoom() const
+void Camera::zoom() 
 {
     if (!ImGui::GetIO().WantCaptureMouse)
     {
