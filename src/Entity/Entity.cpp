93691463195961dc5a052ebdce31a8a3b404a68f
Мурашов Георгий <murashov.george@csoft.ru>
2025-02-22 13:32:41 +0300

    ECS work. First entity

--- src/Entity/Entity.cpp
+++ src/Entity/Entity.cpp
@@ -1,2 +1,67 @@
 #include "stdafx.h"
 #include "Entity.hpp"
+
+#include "../Resource/ResourceManager.hpp"
+
+EntityManager::EntityManager() 
+{
+}
+
+void EntityManager::createEntity(sf::Vector2f pos, const std::string& texture_path)
+{
+    auto entity = registry.create();
+    registry.emplace<Component_Position>(entity, pos);
+    registry.emplace<Component_Velocity>(entity, sf::Vector2f(10.0f, 0.0f));
+
+    sf::Sprite sprite;
+    texture = ResourceLoader::instance().getTexture(texture_path);
+    sprite.setTexture(texture);
+    registry.emplace<Component_Sprite>(entity, sprite);
+}
+
+void EntityManager::destroyEntity(entt::entity entity) 
+{
+    registry.destroy(entity);
+}
+
+void EntityManager::update(float deltaTime) 
+{
+    MovementSystem::update(registry, deltaTime);
+    SpriteUpdateSystem::update(registry);
+}
+
+void EntityManager::draw(sf::RenderWindow& window) 
+{
+    auto view = registry.view<Component_Sprite>();
+    for (auto entity : view) 
+    {
+        const auto& sprite = view.get<Component_Sprite>(entity);
+        window.draw(sprite.sprite);
+    }
+}
+
+
+void MovementSystem::update(entt::registry& registry, float deltaTime) 
+{
+    auto view = registry.view<Component_Position, Component_Velocity>();
+    for (auto entity : view) 
+    {
+        auto& position       = view.get<Component_Position>(entity);
+        const auto& velocity = view.get<Component_Velocity>(entity);
+
+        position.position.x += velocity.velocity.x * deltaTime;
+        position.position.y += velocity.velocity.y * deltaTime;
+    }
+}
+
+void SpriteUpdateSystem::update(entt::registry& registry) 
+{
+    auto spriteView = registry.view<Component_Position, Component_Sprite>();
+    for (auto entity : spriteView) 
+    {
+        auto& sprite         = spriteView.get<Component_Sprite>(entity);
+        const auto& position = spriteView.get<Component_Position>(entity);
+
+        sprite.sprite.setPosition(position.position);
+    }
+}
--- src/Entity/Entity.hpp
+++ src/Entity/Entity.hpp
@@ -1,11 +1,43 @@
 #pragma once
 
-class Entity
+struct Component_Position
+{
+    sf::Vector2f position;
+};
+
+struct Component_Velocity 
+{
+    sf::Vector2f velocity;
+};
+
+
+struct Component_Sprite 
+{
+    sf::Sprite sprite;
+};
+
+class MovementSystem 
 {
 public:
-    Entity();
-    ~Entity();
+    static void update(entt::registry& registry, float deltaTime);
+};
+
+class SpriteUpdateSystem 
+{
+public:
+    static void update(entt::registry& registry);
+};
+
+class EntityManager 
+{
+public:
+    EntityManager();
+    void createEntity(sf::Vector2f pos, const std::string& texture_path);
+    void destroyEntity(entt::entity entity);
+    void update(float delta_time);
+    void draw(sf::RenderWindow& window);
 
 private:
-
+    entt::registry registry;
+    sf::Texture    texture;
 };
