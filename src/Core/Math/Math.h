92f22cecda1d54edeff39719914fa8d08a067fb3
Мурашов Георгий <murashov.george@csoft.ru>
2025-08-15 02:11:31 +0300

    Add new math methods

--- src/Core/Math/Math.h
+++ src/Core/Math/Math.h
@@ -1,23 +1,102 @@
 #pragma once
 
-class Math 
+/**
+ * @brief A utility class providing common mathematical operations.
+ */
+class Math
 {
 public:
-    /// @brief Converts a value to a percentage of another value.
+    /**
+     * @brief Converts a value to a percentage of another value.
+     *
+     * @tparam T1 The type of the base value.
+     * @tparam T2 The type of the percentage.
+     * @param first The base value.
+     * @param percentage The percentage to apply.
+     * @return The calculated percentage of the base value as a float.
+     */
     template <typename T1, typename T2>
     static float convertToPercentage(T1 first, T2 percentage);
 
-    /// @brief Center an object relative to a given position.
+    /**
+     * @brief Calculates the center position relative to given dimensions.
+     *
+     * @tparam T1 The type of the first dimension (e.g., width).
+     * @tparam T2 The type of the second dimension (e.g., height).
+     * @param first The first dimension.
+     * @param second The second dimension.
+     * @return A vector representing the center point.
+     */
     template <typename T1, typename T2>
     static sf::Vector2f setCentre(T1 first, T2 second);
 
-    /// @brief Maps a value from its range to a percentage [0..100].
+    /**
+     * @brief Maps a value from its range to a percentage [0..100].
+     *
+     * @tparam T The numeric type of the value and range bounds.
+     * @param value The value to convert to percentage.
+     * @param range_min The minimum value of the range.
+     * @param range_max The maximum value of the range.
+     * @return The percentage representation of the value within the range.
+     */
     template <typename T>
     static T toPercent(T value, T range_min, T range_max);
 
-    /// @brief Maps a percentage [0..100] back to a value in the given range.
+    /**
+     * @brief Maps a percentage [0..100] back to a value in the given range.
+     *
+     * @tparam T The numeric type of the percentage and range bounds.
+     * @param percentage The percentage to convert (clamped between 0 and 100).
+     * @param range_min The minimum value of the target range.
+     * @param range_max The maximum value of the target range.
+     * @return The value corresponding to the percentage within the range.
+     */
     template <typename T>
     static T fromPercent(T percentage, T range_min, T range_max);
+
+    /**
+     * @brief Normalizes a 2D vector to unit length.
+     *
+     * If the vector's length is close to zero, returns a default unit vector (1, 0).
+     *
+     * @param vector The vector to normalize.
+     * @return The normalized vector, or (1, 0) if the input vector is near-zero.
+     */
+    static sf::Vector2f normalizeVector(const sf::Vector2f& vector);
+
+    /**
+     * @brief Calculates a perpendicular vector (rotated 90 degrees counter-clockwise).
+     *
+     * @param vector The input vector.
+     * @return A vector perpendicular to the input.
+     */
+    static sf::Vector2f perpendicular(const sf::Vector2f& vector);
+
+    /**
+     * @brief Multiplies a vector by a scalar value.
+     *
+     * @param vector The vector to multiply.
+     * @param scalar The scalar multiplier.
+     * @return The resulting vector after multiplication.
+     */
+    static sf::Vector2f multiply(const sf::Vector2f& vector, float scalar);
+
+    /**
+     * @brief Divides a vector by a scalar value.
+     *
+     * @param vector The vector to divide.
+     * @param scalar The scalar divisor.
+     * @return The resulting vector after division.
+     */
+    static sf::Vector2f divide(const sf::Vector2f& vector, float scalar);
+
+    /**
+     * @brief Calculates the length (magnitude) of a 2D vector.
+     *
+     * @param vector The input vector.
+     * @return The Euclidean length of the vector.
+     */
+    static float length(const sf::Vector2f& vector);
 };
 
 #include "Math.inl"
--- src/Core/Math/Math.inl
+++ src/Core/Math/Math.inl
@@ -32,3 +32,32 @@ T Math::fromPercent(T percentage, T range_min, T range_max)
 
     return static_cast<T>(range_min + (range_max - range_min) * (static_cast<float>(percentage) / 100.0f));
 }
+
+inline sf::Vector2f Math::normalizeVector(const sf::Vector2f& vector) 
+{
+    float len = length(vector);
+    if (len > 0.001f) 
+        return vector / len;
+
+    return sf::Vector2f(1.0f, 0.0f);
+}
+
+inline sf::Vector2f Math::perpendicular(const sf::Vector2f& vector) 
+{
+    return sf::Vector2f(-vector.y, vector.x);
+}
+
+inline sf::Vector2f Math::multiply(const sf::Vector2f& vector, float scalar) 
+{
+    return vector * scalar;
+}
+
+inline sf::Vector2f Math::divide(const sf::Vector2f& vector, float scalar)
+{
+    return vector / scalar;
+}
+
+inline float Math::length(const sf::Vector2f& vector)
+{
+    return std::sqrt(vector.x * vector.x + vector.y * vector.y);
+}
