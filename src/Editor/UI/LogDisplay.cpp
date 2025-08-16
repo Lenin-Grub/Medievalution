92a6ce55b5c146287ce0bfdc1eebc3e41b62c6c7
Мурашов Георгий <murashov.george@csoft.ru>
2025-08-16 18:27:45 +0300

    Logs copy

--- src/Editor/UI/LogDisplay.cpp
+++ src/Editor/UI/LogDisplay.cpp
@@ -96,6 +96,7 @@ void LogDisplay::draw()
     ImGui::Checkbox("Critical", &show_critical);
 
     ImGui::SameLine();
+    ImGui::SetNextItemWidth(300.0f);
     ImGui::InputText("Search", search_query, sizeof(search_query));
 
     ImGui::Separator();
@@ -156,6 +157,10 @@ void LogDisplay::drawLog(const LogEntry& entry)
 {
     ImVec4 color = getLevelColor(entry.level);
     
+    ImGui::PushID(static_cast<int>(std::hash<std::string>{}(entry.timestamp + entry.message) & 0x7FFFFFFF));
+    
+    ImGui::BeginGroup();
+    
     if (show_timestamps)
     {
         ImGui::TextColored(ImVec4(0.7f, 0.7f, 0.7f, 1.0f), "[%s]", entry.timestamp.c_str());
@@ -165,6 +170,83 @@ void LogDisplay::drawLog(const LogEntry& entry)
     ImGui::TextColored(color, "[%s]", getLevelName(entry.level));
     ImGui::SameLine();
     ImGui::TextWrapped("%s", entry.message.c_str());
+    
+    ImGui::EndGroup();
+    
+    if (ImGui::BeginPopupContextItem("LogEntryContextMenu"))
+    {
+        if (ImGui::Selectable("Copy Line"))
+        {
+            std::string full_line;
+            if (show_timestamps)
+            {
+                full_line = fmt::format("[{}] [{}] {}", entry.timestamp, getLevelName(entry.level), entry.message);
+            }
+            else
+            {
+                full_line = fmt::format("[{}] {}", getLevelName(entry.level), entry.message);
+            }
+            ImGui::SetClipboardText(full_line.c_str());
+        }
+        
+        if (ImGui::Selectable("Copy All"))
+        {
+            std::string all_logs;
+            auto logs = log_sink->get_logs();
+            
+            for (const auto& log_entry : logs)
+            {
+                bool should_show = false;
+                switch (log_entry.level)
+                {
+                    case spdlog::level::debug:
+                        should_show = show_debug;
+                        break;
+                    case spdlog::level::info:
+                        should_show = show_info;
+                        break;
+                    case spdlog::level::warn:
+                        should_show = show_warn;
+                        break;
+                    case spdlog::level::err:
+                        should_show = show_error;
+                        break;
+                    case spdlog::level::critical:
+                        should_show = show_critical;
+                        break;
+                    default:
+                        should_show = true;
+                        break;
+                }
+
+                if (!should_show)
+                    continue;
+
+                if (search_query[0] != '\0' && log_entry.message.find(search_query) == std::string::npos)
+                    continue;
+
+                if (show_timestamps)
+                {
+                    all_logs += fmt::format("[{}] [{}] {}\n", log_entry.timestamp, getLevelName(log_entry.level), log_entry.message);
+                }
+                else
+                {
+                    all_logs += fmt::format("[{}] {}\n", getLevelName(log_entry.level), log_entry.message);
+                }
+            }
+            
+            if (!all_logs.empty() && all_logs.back() == '\n') 
+            {
+                all_logs.pop_back();
+            }
+            
+            ImGui::SetClipboardText(all_logs.c_str());
+        }
+        
+        ImGui::EndPopup();
+    }
+    
+    ImGui::PopID();
 }
 
 ImVec4 LogDisplay::getLevelColor(spdlog::level::level_enum level)
